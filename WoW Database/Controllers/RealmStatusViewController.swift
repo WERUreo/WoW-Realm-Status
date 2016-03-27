fc0f33090d90c6457437d52eebcc21557c2576be
Keli'i Martin <kelii.d.martin@gmail.com>
2016-03-27 00:59:43 -0400

    Set up sections for realm tableview
    The list of realms is now grouped into sections based on the first letter of the realm name.

--- WoW Database/Controllers/RealmStatusViewController.swift
+++ WoW Database/Controllers/RealmStatusViewController.swift
@@ -17,6 +17,7 @@ class RealmStatusViewController: UIViewController, UITableViewDelegate, UITableV
 
     let realmSegueIdentifier = "RealmDetailSegue"
     var realms = [Realm]()
+    var sections: [(index: Int, length: Int, title: String)] = Array()
 
     ////////////////////////////////////////////////////////////
 
@@ -26,8 +27,9 @@ class RealmStatusViewController: UIViewController, UITableViewDelegate, UITableV
         tableView.delegate = self
         tableView.dataSource = self
 
-        self.canDisplayBannerAds = true
+        canDisplayBannerAds = true
 
+        // FIXME: - remove this from the view controller
         let parameters =
         [
             "locale" : "en-US",
@@ -44,6 +46,21 @@ class RealmStatusViewController: UIViewController, UITableViewDelegate, UITableV
                     for (_, subJson) in json["realms"]
                     {
                         self.realms.append(Realm(json: subJson))
+                    }
+
+                    var index = 0
+                    for i in 0..<self.realms.count
+                    {
+                        let commonPrefix = self.realms[i].name.commonPrefixWithString(self.realms[index].name, options: .CaseInsensitiveSearch)
+                        if commonPrefix.characters.count == 0
+                        {
+                            let string = self.realms[index].name.uppercaseString
+                            let firstCharacter = string[string.startIndex]
+                            let title = "\(firstCharacter)"
+                            let newSection = (index: index, length: i - index, title: title)
+                            self.sections.append(newSection)
+                            index = i
+                        }
                     }
 
                     self.tableView.reloadData()
@@ -76,21 +93,21 @@ class RealmStatusViewController: UIViewController, UITableViewDelegate, UITableV
 
     func numberOfSectionsInTableView(tableView: UITableView) -> Int
     {
-        return 1
+        return sections.count
     }
 
     ////////////////////////////////////////////////////////////
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return realms.count
+        return sections[section].length
     }
 
     ////////////////////////////////////////////////////////////
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
     {
-        let realm = realms[indexPath.row]
+        let realm = realms[sections[indexPath.section].index + indexPath.row]
         let cell = tableView.dequeueReusableCellWithIdentifier("RealmCell", forIndexPath: indexPath) as? RealmCell
         if let realmCell = cell
         {
@@ -101,6 +118,27 @@ class RealmStatusViewController: UIViewController, UITableViewDelegate, UITableV
         {
             return RealmCell()
         }
+    }
+
+    ////////////////////////////////////////////////////////////
+
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String?
+    {
+        return sections[section].title
+    }
+
+    ////////////////////////////////////////////////////////////
+
+    func sectionIndexTitlesForTableView(tableView: UITableView) -> [String]?
+    {
+        return sections.map { $0.title }
+    }
+
+    ////////////////////////////////////////////////////////////
+
+    func tableView(tableView: UITableView, sectionForSectionIndexTitle title: String, atIndex index: Int) -> Int
+    {
+        return index
     }
 
     ////////////////////////////////////////////////////////////
