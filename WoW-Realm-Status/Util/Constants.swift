0e370a1b0aecda3e0ff535b19f07eca0e9a0f385
Keli'i Martin <kelii.d.martin@gmail.com>
2016-04-10 20:42:38 -0400

    Add/remove favorites complete

---
+++ WoW-Realm-Status/Util/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  WoW-Realm-Status
+//
+//  Created by Keli'i Martin on 4/10/16.
+//  Copyright © 2016 WERUreo. All rights reserved.
+//
+
+import Foundation
+
+let FAVORITE_REALMS_KEY = "Favorites"
+let USER_DEFAULTS = NSUserDefaults.standardUserDefaults()
